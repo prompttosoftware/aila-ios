b3927170b35fa7392c7f339a9bbbfeab7a66e9da
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-10-17 08:10:13 -0500

    Setup: template copy

---
+++ AilaIos/AilaIosTests/AilaIosTests.swift
@@ -0,0 +1,14 @@
+//
+//  AilaIosTests.swift
+//  AilaIosTests
+//
+
+import Testing
+
+struct AilaIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
